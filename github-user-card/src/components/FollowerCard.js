7374ec95f2f1edafceb3c97c3fd1c8a36698637f
charlene-johnson <charlene.j8234@gmail.com>
2020-06-09 02:35:45 -0400

    Finished the followercard component, added some headings to the usercard component to make it easier to read

--- github-user-card/src/components/FollowerCard.js
+++ github-user-card/src/components/FollowerCard.js
@@ -3,7 +3,16 @@ import React from 'react';
 class FollowerCard extends React.Component {
     render() {
         return(
-            
+            <div className="followerContainer">
+                {this.props.followers.map(follower =>
+                    <div className="followerCard">
+                        <img src={follower.avatar_url}/>
+                        <p className="followerUserName">Follower Username: {follower.login}</p>
+                        </div>
+                        )}
+            </div>
         )
     }
 }
+
+export default FollowerCard;
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -8,12 +8,12 @@ class UserCard extends React.Component {
                 <div className="userCard">
                     <img src={this.props.users.avatar_url}/>
                         <h2>{this.props.users.name}</h2>
-                        <p className="userName">{this.props.users.login}</p>
-                        <p className="location">{this.props.users.location}</p>
-                        <p className="profile">{this.props.users.html_url}</p>
-                        <p className="followers">{this.props.users.followers}</p>
-                        <p className="following">{this.props.users.following}</p>
-                        <p className="bios">{this.props.users.bios}</p>
+                        <p className="userName">Username: {this.props.users.login}</p>
+                        <p className="location">Location: {this.props.users.location}</p>
+                        <p className="profile">Profile: {this.props.users.html_url}</p>
+                        <p className="followers">Followers: {this.props.users.followers}</p>
+                        <p className="following">Following: {this.props.users.following}</p>
+                        <p className="bios">Bio: {this.props.users.bios}</p>
                 </div>
             </div>
         )
